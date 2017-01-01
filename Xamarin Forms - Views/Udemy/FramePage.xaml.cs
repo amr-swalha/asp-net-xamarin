e39c134372e56e11abfcc16a383af09e2a5d3876
amr-swalha <amr.swalha.code@outlook.com>
2017-01-01 12:29:38 +0200

    Adding the Xamarin Views Code

---
+++ Xamarin Forms - Views/Udemy/FramePage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace Udemy
+{
+    public partial class FramePage : ContentPage
+    {
+        public FramePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Xamarin Forms - Views/Udemy/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace Udemy
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void BtnContentView(object sender,EventArgs e)
+        {
+            App.Current.MainPage = new NavigationPage(new ContentViewPage());
+        }
+        private void BtnFrame(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new NavigationPage(new FramePage());
+        }
+        private void BtnScroll(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new NavigationPage(new ScrollPage());
+        }
+        private void BtnGrid(object sender, EventArgs e)
+        {
+            App.Current.MainPage = new NavigationPage(new GridPage());
+        }
+    }
+}
