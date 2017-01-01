ca316e845e25c6fc40756d49f34022bcf70a5be0
amr-swalha <amr.swalha.code@outlook.com>
2017-01-01 13:18:34 +0200

    UI Testing

---
+++ Xamarin  - UI Test/UITestXamarin/AppInitializer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace UITestXamarin
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp
+                    .Android
+                    //.DeviceSerial("HQ695BT80857")
+                    .ApkFile(@"C:\Users\afswa\Documents\Visual Studio 2015\Projects\XamarinForms\Udemy\Udemy.Droid\bin\Release\Udemy.Droid.apk")
+                    .StartApp();
+            }
+
+            return ConfigureApp
+                .iOS
+                .StartApp();
+        }
+    }
+}
+
---
+++ Xamarin  - UI Test/UITestXamarin/Tests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace UITestXamarin
+{
+    [TestFixture(Platform.Android)]
+    //[TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void AppLaunches()
+        {
+            app.Screenshot("First screen.");
+        }
+        [Test]
+        public void TabContent()
+        {
+            app.Tap(c => c.Marked("Content View"));
+            //app.WaitFor(c => c.Marked("BtnContent"), )
+        }
+        [Test]
+        public void TabContent2()
+        {
+            app.Tap(c => c.Marked("Content View"));
+            //app.WaitFor(c => c.Marked("BtnContent"), )
+        }
+    }
+}
+
